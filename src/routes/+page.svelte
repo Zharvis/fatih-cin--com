628cdf99b37e7ea7314668a9512b2a23c80fbf29
Fatih <fatih.mehmed.cin@gmail.com>
2024-11-25 19:02:41 +0100

    initial commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="wrapper">
+	<h1>fatih-cin.com</h1>
+	<p>coming soon...</p>
+</div>
+
+<style>
+	.wrapper {
+		font-family: 'Courier New', Courier, monospace;
+		text-align: center;
+		height: 95vh;
+		align-content: center;
+	}
+</style>
